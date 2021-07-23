f023fad724c00c2dad5f698af9aec88aef06e74f
Niloy Majumder <niloymajumder.98@gmail.com>
2021-07-23 19:32:30 +0530

    A note Taking App using ReactJS

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [expand, setExpand] = useState(false);
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    setExpand(false);
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {expand && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          onClick={() => {
+            setExpand(true);
+          }}
+          rows={expand ? 3 : 1}
+        />
+        <Zoom in={expand}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
